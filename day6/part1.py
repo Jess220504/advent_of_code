eead7868072a1c49697cb5d6a01195608abe2ed5
Jess220504 <jessygungadin220504@gmail.com>
2025-12-13 15:35:17 +0100

    finished day 6 part 1

--- day6/part1.py
+++ day6/part1.py
@@ -1,18 +1,26 @@
-with open("day6/test.txt", "r") as file:
+with open("day6/doc.txt", "r") as file:
     lines = file.readlines()
 res = 0
-new_l = []
+new_tab = []
 
 for line in lines:
+    new_line = []
     splitted_line = line.split(" ")
     for i in range(len(splitted_line)):
         if splitted_line[i] != "" and splitted_line[i] != "\n":
             if "\n" in splitted_line[i]:
-                new_l.append(splitted_line[i][:-1])
+                new_line.append(splitted_line[i][:-1])
             else:
-                new_l.append(splitted_line[i])
+                new_line.append(splitted_line[i])
+    new_tab.append(new_line)
 
-print(new_l, len(lines))
-
-for i in range(len(new_l) // len(lines) - 1):
-    print("calculs")
+for i in range(len(new_tab[0])):
+    col_res = int(new_tab[0][i])
+    if new_tab[-1][i] == "*":
+        for j in range(1, len(new_tab) - 1):
+            col_res *= int(new_tab[j][i])
+    else:
+        for j in range(1, len(new_tab) - 1):
+            col_res += int(new_tab[j][i])
+    res += col_res
+print(res)
